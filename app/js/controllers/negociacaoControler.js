fd8cca381627fa7a1ad8ab9e58af44270ce03012
Valmyr Tavares <valmyr.lima@millennium.com.br>
2019-05-31 17:36:08 -0300

    Começando

---
+++ app/js/controllers/negociacaoControler.js
@@ -0,0 +1,12 @@
+class NegociacaoControler {
+    constructor() {
+        this._inputData = document.querySelector("#data");
+        this._inputData = document.querySelector("#quantidade");
+        this._inputValor = document.querySelector("#valor");
+    }
+    adiciona(event) {
+        event.preventDefault();
+        const negociacao = new Negociacao(this._inputData.value, this._inputQuantidade.value, this._inputValor.value);
+        console.log(negociacao);
+    }
+}
---
+++ app/ts/app.ts
@@ -0,0 +1,6 @@
+const controller = new NegociacaoControler();
+document
+    .querySelector('.form')
+
+    .addEventListener('submit', controller.adiciona.bind(controller));
+console.log(negociacao);
